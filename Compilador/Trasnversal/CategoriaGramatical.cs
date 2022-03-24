d576d1153a5931d3d5741d645c3d62d90bda81c7
abedoyaf18 <60907572+abedoyaf18@users.noreply.github.com>
2022-03-23 23:34:54 -0500

    Analizador Lexico

--- Compilador/Trasnversal/CategoriaGramatical.cs
+++ Compilador/Trasnversal/CategoriaGramatical.cs
@@ -19,5 +19,12 @@ namespace CompiladorClase.Trasnversal
         public static string PARENTESIS_ABRE = "PARENTESIS ABRE";
         public static string PARENTESIS_CIERRA = "PARENTESIS CIERRA";
         public static string BLANCO = "BLANCO";
+        public static string MODULO = "MODULO";
+        public static string IGUAL = "IGUAL";
+        public static string DIFERENTE_QUE = "DIFERENTE QUE";
+        public static string MAYOR_QUE = "MAYOR QUE";
+        public static string MENOR_QUE = "MENOR QUE";
+        public static string MENOR_O_IGUAL_QUE = "MENOR O IGUAL QUE";
+        public static string MAYOR_O_IGUAL_QUE = "MAYOR O IGUAL QUE";
     }
 }
